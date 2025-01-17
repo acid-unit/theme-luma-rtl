59b8c238c8c071a10d01ebeb48dd10ab5d2519a0
Constantine Gosteev <gosteev00@gmail.com>
2025-01-17 14:42:41 +0200

    init

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Acid Unit (https://acid.7prism.com). All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AcidUnit/luma-rtl', __DIR__);
